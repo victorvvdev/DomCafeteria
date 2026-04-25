d3b2fa8257cf5f4de092ce076776fb1f7aec2f74
Victor Viana <victor.vviana02@gmail.com>
2026-04-25 16:41:58 -0300

    Resolve merge conflict in AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,9 @@ import SobreNos from "../pages/publicas/SobreNos";
 import Contato from "../pages/publicas/Contato";
 import Login from "../pages/admin/Login";
 import RecuperarSenha from "../pages/admin/RecuperarSenha";
+import ContatoAdm from "../pages/admin/ContatoAdm";
+import AdicionarAdm from "../pages/admin/AdicionarAdm";
+import InicioAdm from "../pages/admin/InicioAdm";
 
 function AppRoutes() {
   return (
@@ -13,8 +16,13 @@ function AppRoutes() {
       <Route path="/cardapio" element={<Cardapio />} />
       <Route path="/sobre" element={<SobreNos />} />
       <Route path="/contato" element={<Contato />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+
+      <Route path="/adm/inicioadm" element={<InicioAdm />} />
+      <Route path="/adm/contato" element={<ContatoAdm />} />
+      <Route path="/adm/adicionar" element={<AdicionarAdm />} />
     </Routes>
   );
 }
