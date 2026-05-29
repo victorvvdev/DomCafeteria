63ba709118f0a3a962a61dd45fcb0d1e8616efc0
João Pedro Floriano <joaopedrofloriano21@gmail.com>
2026-05-29 13:17:19 -0300

    Update vite.config.js

--- Dom-Cafeteria/vite.config.js
+++ Dom-Cafeteria/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  appType: 'spa',
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
 })
